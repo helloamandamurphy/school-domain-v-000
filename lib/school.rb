653a5080b66baf22010f60c297c6c1a992754700
Amanda Murphy <helloamandamurphy@gmail.com>
2018-10-10 19:38:45 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,13 +1,19 @@
 class School
+  #attr_accessor :student_name, :grade,
+  #attr_reader :school_name
+  def roster
+    roster = {}
+  end
+  
   def initialize(school_name)
-    #def roster
-      #roster = {}
-    #end
     school_name= (school_name)
     @school_name = school_name
+    roster
   end
   
   def add_student(student_name, grade)
+    roster[grade] = []
+    roster[grade] << [student_name]
   end
   
   def grade
