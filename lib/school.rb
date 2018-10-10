a52ef399cd85231c71a6370485c9010363129136
Amanda Murphy <helloamandamurphy@gmail.com>
2018-10-10 19:55:30 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -11,8 +11,8 @@ class School
   end
   
   def add_student(student_name, grade)
-    roster[grade] = []
-    roster[grade] << [student_name]
+    @roster[grade] = []
+    @roster[grade] << [student_name]
   end
   
   def grade
